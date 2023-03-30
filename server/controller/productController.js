7211d90b5450aad12b51a75e78e0367b14b4c447
deepti1144 <dipz09085@gmail.com>
2023-03-30 18:26:00 +0530

    working on orderROute

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -2,6 +2,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Product = require("../model/productModel");
 const ApiFeatures = require("../utils/apifeature");
 const product=require("../db.json");
+const ErrorHandler = require("../utils/errorHandler");
 
 // {====================Create-Product========= ADMIN  ++++=================}
 exports.createProduct = catchAsyncErrors(async (req, res) => {
@@ -43,6 +44,40 @@ exports.getAllProducts = catchAsyncErrors(async (req, res) => {
 exports.getSingleProduct=catchAsyncErrors(async(req,res)=>{
    const product=await Product.findById(req.params.id);
    res.status(201).json({
+    success: true,
+    product,
+  });
+});
+
+// {========================DELETE-PRODUCT--------------------------===================}
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Product Delete Successfully",
+  });
+});
+
+// {========================UPDATE-PRODUCT----------ADMIN----------------===================}
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
     success: true,
     product,
   });
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { createProduct, getAllProducts, getSingleProduct } = require("../controller/productController");
+const { createProduct, getAllProducts, getSingleProduct, deleteProduct, updateProduct } = require("../controller/productController");
 const { requireSignIn, isAdmin } = require("../middleware/authMiddleware");
 
 const productRoute=express.Router();
@@ -9,5 +9,9 @@ productRoute.post("/create",requireSignIn, isAdmin,createProduct)
 productRoute.get("/getproducts",getAllProducts)
 
 productRoute.get("/getproducts/:id",getSingleProduct)
+
+productRoute.delete("/getproducts/:id",requireSignIn, isAdmin, deleteProduct)
+
+productRoute.put("/getproducts/:id",requireSignIn, isAdmin,updateProduct)
 
 module.exports=productRoute;
