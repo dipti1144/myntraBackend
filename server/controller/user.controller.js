f23679583ddeca685b3100c351806257404c4523
deepti1144 <dipz09085@gmail.com>
2023-04-02 20:02:02 +0530

    login error rectify

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -85,7 +85,7 @@ exports.loginUser=async(req,res)=>{
         }
         const match = await comparePassword(password, user.password);
         if (!match) {
-          return res.status(200).send({
+          return res.status(400).send({
             success: false,
             message: "Invalid Password",
           });
