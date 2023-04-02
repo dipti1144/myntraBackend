279bf56a991d701254f37d39eb523a439309342e
deepti1144 <dipz09085@gmail.com>
2023-04-02 20:24:39 +0530

    done

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -4,7 +4,7 @@ const UserSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  gender: { type: String, required: true },
+  gender: { type: String },
   number: { type: Number, required: true },
   role:{type:Number,default:0}
 });
