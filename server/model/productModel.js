afecb847b6428699da9d7d5e129fc01b1bcd2288
deepti1144 <dipz09085@gmail.com>
2023-04-02 21:21:03 +0530

    done

--- server/model/productModel.js
+++ server/model/productModel.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const ProductSchema=mongoose.Schema({
     title:{type:String, require:true},
     brand:{type:String, require:true},
-    category:{type:String, require:true},
+    category:{type:String, require:false},
     discounted_price:{type:Number, require:true},
     strike_price:{type:Number, require:true},
     discount:{type:String, require:true},
-    rating:{type:Number, require:true},
-    rating_count:{type:String, require:true},
+    rating:{type:Number, require:false},
+    rating_count:{type:String, require:false},
     images: {type:String, require:true},
-    size:{type:Array , require:true} ,
+    size:{type:Array , require:false} ,
     user: {
         type: mongoose.Schema.ObjectId,
         ref: "user",
